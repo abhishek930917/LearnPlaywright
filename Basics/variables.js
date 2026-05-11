b54b33226c8b9d0385009a009eaa991a3b6b7f15
User <user@example.com>
2026-05-11 13:10:19 +0530

    Initial commit

---
+++ Basics/variables.js
@@ -0,0 +1,18 @@
+var a=10;
+let b = 20;
+const c = 30;
+console.log(a);
+console.log(b);
+console.log(c); 
+
+if(true){
+    var a = 40;
+    let b = 50;
+    const c = 60;
+    console.log(a);
+    console.log(b);
+    console.log(c); 
+}
+console.log(a);
+console.log(b);
+console.log(c);
